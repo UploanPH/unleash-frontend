c7adcc923fa7841e1180a7233e194e948b132ec6
Ryan Ermita <ryan.ermita@uploan.ph>
2021-02-15 11:35:33 +0800

    make sure stellar auth is loaded

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -8,6 +8,32 @@ import { routes } from './menu/routes';
 import styles from './styles.module.scss';
 import AuthenticationContainer from './user/authentication-container';
 import MainLayout from './layout/main';
+import { Spinner } from 'react-mdl';
+import { setStellarOauthToken, timeout } from '../store/api-helper'
+import { fetchUser } from '../store/user/actions';
+
+
+const customStyles = {
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        zIndex: 99999,
+    },
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        backgroundColor: 'transparent',
+        padding: 0,
+        overflow: 'none',
+    },
+};
 
 class App extends PureComponent {
     static propTypes = {
@@ -17,15 +43,33 @@ class App extends PureComponent {
         user: PropTypes.object,
     };
 
+    state = {
+        isStellarAuthLoaded: false
+    }
+
+    async componentDidMount() {
+        setStellarOauthToken()
+        await timeout(3000)
+        this.setState({
+            isStellarAuthLoaded: true
+        })
+    }
+
     render() {
-        if (this.props.user.authDetails) {
-            return <AuthenticationContainer history={this.props.history} />;
+        let { isStellarAuthLoaded } = this.state
+
+        if (!isStellarAuthLoaded) {
+            return (
+                <div style={customStyles.overlay}>
+                    <Spinner style={customStyles.content} />
+                </div>
+            )
         }
         return (
             <div className={styles.container}>
                 <MainLayout {...this.props}>
                     <Switch>
-                        <Route exact path="/" />
+                        <Route exact path="/" render={() => <Redirect to="/features" component={Features} />} />
                         {routes.map(route => (
                             <Route key={route.path} path={route.path} component={route.component} />
                         ))}
