9669aaff518796bdf286da1b7ca3a0a20bfc92e6
Ryan Ermita <ryanermita@users.noreply.github.com>
2021-02-16 13:42:46 +0800

    ISVC-68 Feature Flag Service and Stellar Integration

--- src/index.jsx
+++ src/index.jsx
@@ -16,6 +16,9 @@ import App from './component/app';
 import ScrollToTop from './component/scroll-to-top';
 import { writeWarning } from './security-logger';
 
+import { retrieveTokenBearer, setStellarOauthToken, retrieveOAuthDetails, timeout } from './store/api-helper'
+import fetchIntercept from 'fetch-intercept';
+
 let composeEnhancers;
 
 if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
@@ -28,6 +31,33 @@ if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_C
 const unleashStore = createStore(store, composeEnhancers(applyMiddleware(thunkMiddleware)));
 const metricsPoller = new MetricsPoller(unleashStore);
 metricsPoller.start();
+
+const unregister = fetchIntercept.register({
+    request: async function (url, config) {
+        const oauthDetails = retrieveOAuthDetails()
+        if (Object.keys(oauthDetails).length === 0) {
+            setStellarOauthToken()
+            await timeout(3000)
+        }
+        if (url === process.env.UPLOAN_AUTH_URL) {
+            return [url, config];
+        }
+
+        let token = await retrieveTokenBearer()
+        if (typeof config.headers === 'undefined') {
+            config.headers = {}
+        }
+        config.headers["Authorization"] = `Bearer ${token}`
+        config.headers["Accept"] = 'application/json'
+        config.headers["Content-Type"] = 'application/json';
+
+        return [url, config];
+    },
+
+    requestError: err => Promise.reject(err),
+    response: res => res,
+    responseError: err => Promise.reject(err)
+});
 
 ReactDOM.render(
     <Provider store={unleashStore}>
--- src/store/api-helper.js
+++ src/store/api-helper.js
@@ -1,3 +1,6 @@
+import { refreshUploanOAuthToken } from '../store/user/actions'
+import jwtDecode from 'jwt-decode'
+
 const defaultErrorMessage = 'Unexpected exception when talking to unleash-api';
 
 function extractJoiMsg(body) {
@@ -75,3 +78,57 @@ export const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
 };
+
+export const uploanUserToUnleashUser = (uploanUser) => {
+    return {
+        email: uploanUser.user_name,
+        permission: uploanUser.permissions,
+        type: uploanUser.type,
+        roles: uploanUser.roles
+    }
+}
+
+// this helper method connects Stellar Service and Unleash Front-end
+export const iframeEventBinder = (element, eventName, eventHandler) => {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, eventHandler, false)
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + eventName, eventHandler)
+    }
+}
+
+// async settimeout
+export const timeout = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const setStellarOauthToken = () => {
+    iframeEventBinder(window, 'message', (e) => {
+        if (typeof e.data === 'string' && e.data) {
+            sessionStorage.setItem('oauth', e.data)
+        }
+    })
+}
+
+export const retrieveOAuthDetails = () => {
+    return sessionStorage.getItem('oauth') ? JSON.parse(sessionStorage.getItem('oauth')) : {};
+}
+
+export const retrieveTokenBearer = async () => {
+    const oauthDetails = retrieveOAuthDetails()
+    if (Object.keys(oauthDetails).length === 0) {
+        return '';
+    }
+
+    const decodedAccessToken = jwtDecode(oauthDetails.access_token)
+    const accessTokenExpDate = new Date(decodedAccessToken.exp * 1000)
+    const currentDate = new Date()
+    if (currentDate > accessTokenExpDate) {
+        const refreshTokenParams = { refresh_token: oauthDetails.refresh_token }
+        await refreshUploanOAuthToken(process.env.UPLOAN_AUTH_URL, refreshTokenParams)
+
+        const refreshedOAuthDetails = retrieveOAuthDetails()
+        return refreshedOAuthDetails.access_token;
+    }
+    return oauthDetails.access_token;
+}
